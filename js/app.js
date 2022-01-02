21c3f6b03c58cf8b8e4eb3601185f2666e788632
Ramesh Maurya <798mauryaramesh@gamil.com>
2022-01-02 20:25:03 +0530

    added all other template

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
+const loatTemplates=(parent)=>{
+
+    //Find all tags which have the attribute template
+    const items=parent.querySelectorAll('[template]');
+
+    ///iterate through all elements which the query has found
+    items.forEach(item=>{
+        const teplate=item.getAttribute('template');
+
+
+        ///fetch the html content from the server
+        fetch(template)
+
+        //handle a successful response
+
+        .then((response)=>{
+            return response.text();
+        })
+        .then(content=>{
+            ///assign the html content loaded to 
+            //the items innerHTML
+
+            item.innerHTML=content;
+
+            ///run this command on the item element so as to find any child template
+
+            loadTemplates(item);
+        })
+    });
+
+}
+
+
+
 const onWindowLoaded=()=>{
-    console.log('window loaded');
+    loadTemplates(document);
 }
 window.onload=onWindowLoaded;
